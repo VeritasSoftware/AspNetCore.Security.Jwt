c118020954c4d9b14bdde823c0525b3207b85a01
VeritasSoftware <shantanu_negi@hotmail.com>
2018-11-18 09:11:51 +1100

    improved AddSecurityBuilder.

--- AspNetCore.Security.Jwt/Implementations/AddSecurityBuilder.cs
+++ AspNetCore.Security.Jwt/Implementations/AddSecurityBuilder.cs
@@ -12,6 +12,10 @@ namespace AspNetCore.Security.Jwt
     {
         private static SecuritySettings SecuritySettings;
         private static bool IsJwtSchemeAdded = false;
+        private static bool IsDefaultAdded = false;
+        private static bool IsCustomAdded = false;
+        private static bool IsFacebookAdded = false;
+        private static bool IsAzureAdded = false;
         private static IServiceCollection Services;
 
         public AddSecurityBuilder(SecuritySettings securitySettings, bool isJwtSchemeAdded, IServiceCollection services)
@@ -23,89 +27,109 @@ namespace AspNetCore.Security.Jwt
 
         public IAddSecurityBuilder AddAzureADSecurity(bool addSwaggerSecurity = false)
         {
-            IdTypeHelpers.LoadClaimTypes();
-
-            Services.AddSingleton(SecuritySettings);
-            Services.AddSingleton<AzureADSecuritySettings>(SecuritySettings.AzureADSecuritySettings);
-            Services.AddScoped<IAuthentication<AzureADAuthModel, AzureADResponseModel>, AzureAuthenticator>();
-
-            if (addSwaggerSecurity)
+            if (!IsAzureAdded)
             {
-                Services.AddSecureSwaggerDocumentation();
-            }
+                IdTypeHelpers.LoadClaimTypes();
+
+                Services.AddSingleton(SecuritySettings);
+                Services.AddSingleton<AzureADSecuritySettings>(SecuritySettings.AzureADSecuritySettings);
+                Services.AddScoped<IAuthentication<AzureADAuthModel, AzureADResponseModel>, AzureAuthenticator>();
+
+                if (addSwaggerSecurity)
+                {
+                    Services.AddSecureSwaggerDocumentation();
+                }
+
+                IsAzureAdded = true;
+            }            
 
             return this;
         }
 
         public IAddSecurityBuilder AddFacebookSecurity(Action<IIdTypeBuilder<FacebookAuthModel>> addClaims = null, bool addSwaggerSecurity = false)
         {
-            IdTypeHelpers.LoadClaimTypes();
-
-            Services.AddSingleton<BaseSecuritySettings>(SecuritySettings);
-            if (addClaims != null)
+            if (!IsFacebookAdded)
             {
-                Services.AddSingleton<Action<IIdTypeBuilder<FacebookAuthModel>>>(x => addClaims);
-            }
-            Services.AddScoped<ISecurityService<FacebookAuthModel>, SecurityService<FacebookAuthModel>>();
-            Services.AddScoped<IAuthentication<FacebookAuthModel>, FacebookAuthenticator>();
+                IdTypeHelpers.LoadClaimTypes();
 
-            if (addSwaggerSecurity)
-            {
-                Services.AddSecureSwaggerDocumentation();
-            }
+                Services.AddSingleton<BaseSecuritySettings>(SecuritySettings);
+                if (addClaims != null)
+                {
+                    Services.AddSingleton<Action<IIdTypeBuilder<FacebookAuthModel>>>(x => addClaims);
+                }
+                Services.AddScoped<ISecurityService<FacebookAuthModel>, SecurityService<FacebookAuthModel>>();
+                Services.AddScoped<IAuthentication<FacebookAuthModel>, FacebookAuthenticator>();
 
-            if (!IsJwtSchemeAdded)
-            {
-                Services.AddJwtBearerScheme(SecuritySettings);
-                IsJwtSchemeAdded = true;
-            }
+                if (addSwaggerSecurity)
+                {
+                    Services.AddSecureSwaggerDocumentation();
+                }
+
+                if (!IsJwtSchemeAdded)
+                {
+                    Services.AddJwtBearerScheme(SecuritySettings);
+                    IsJwtSchemeAdded = true;
+                }
+
+                IsFacebookAdded = true;
+            }            
 
             return this;
         }
 
         IAddSecurityBuilder IAddSecurityBuilder.AddSecurity<TAuthenticator>(bool addSwaggerSecurity)
         {
-            IdTypeHelpers.LoadClaimTypes();
-
-            Services.AddScoped<ISecurityService, SecurityService>();
-            Services.AddScoped<IAuthentication, TAuthenticator>();
-
-            if (addSwaggerSecurity)
+            if (!IsDefaultAdded && !IsCustomAdded)
             {
-                Services.AddSecureSwaggerDocumentation();
-            }
+                IdTypeHelpers.LoadClaimTypes();
 
-            if (!IsJwtSchemeAdded)
-            {
-                Services.AddJwtBearerScheme(SecuritySettings);
-                IsJwtSchemeAdded = true;
-            }
+                Services.AddScoped<ISecurityService, SecurityService>();
+                Services.AddScoped<IAuthentication, TAuthenticator>();
+
+                if (addSwaggerSecurity)
+                {
+                    Services.AddSecureSwaggerDocumentation();
+                }
+
+                if (!IsJwtSchemeAdded)
+                {
+                    Services.AddJwtBearerScheme(SecuritySettings);
+                    IsJwtSchemeAdded = true;
+                }
+
+                IsDefaultAdded = true;
+            }            
 
             return this;
         }
 
         IAddSecurityBuilder IAddSecurityBuilder.AddSecurity<TAuthenticator, TUserModel>(Action<IIdTypeBuilder<TUserModel>> addClaims, bool addSwaggerSecurity)
         {
-            IdTypeHelpers.LoadClaimTypes();
-
-            Services.AddSingleton(SecuritySettings);
-            Services.AddSingleton<BaseSecuritySettings>(SecuritySettings);
-            if (addClaims != null)
+            if (!IsDefaultAdded && !IsCustomAdded)
             {
-                Services.AddSingleton<Action<IIdTypeBuilder<TUserModel>>>(x => addClaims);
-            }
-            Services.AddScoped<ISecurityService<TUserModel>, SecurityService<TUserModel>>();
-            Services.AddScoped<IAuthentication<TUserModel>, TAuthenticator>();
+                IdTypeHelpers.LoadClaimTypes();
 
-            if (addSwaggerSecurity)
-            {
-                Services.AddSecureSwaggerDocumentation();
-            }
+                Services.AddSingleton(SecuritySettings);
+                Services.AddSingleton<BaseSecuritySettings>(SecuritySettings);
+                if (addClaims != null)
+                {
+                    Services.AddSingleton<Action<IIdTypeBuilder<TUserModel>>>(x => addClaims);
+                }
+                Services.AddScoped<ISecurityService<TUserModel>, SecurityService<TUserModel>>();
+                Services.AddScoped<IAuthentication<TUserModel>, TAuthenticator>();
 
-            if (!IsJwtSchemeAdded)
-            {
-                Services.AddJwtBearerScheme(SecuritySettings);
-                IsJwtSchemeAdded = true;
+                if (addSwaggerSecurity)
+                {
+                    Services.AddSecureSwaggerDocumentation();
+                }
+
+                if (!IsJwtSchemeAdded)
+                {
+                    Services.AddJwtBearerScheme(SecuritySettings);
+                    IsJwtSchemeAdded = true;
+                }
+
+                IsCustomAdded = true;
             }
 
             return this;
