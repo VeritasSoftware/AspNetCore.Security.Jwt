7048bed652ae56f9cb63144699ee8ceb15f2c9fc
VeritasSoftware <shantanu_negi@hotmail.com>
2018-12-01 10:19:42 +1100

    added security extensions unit tests.

---
+++ AspNetCore.Security.Jwt.UnitTests/SecurityExtensionsTests.cs
@@ -0,0 +1,127 @@
+﻿using AspNetCore.Security.Jwt.AzureAD;
+using AspNetCore.Security.Jwt.Facebook;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace AspNetCore.Security.Jwt.UnitTests
+{
+    /// <summary>
+    /// Tests to verify that the Security Extensions are registering the DI properly.
+    /// </summary>
+    public class SecurityExtensionsTests
+    {
+        IConfiguration Configuration { get; set; }
+
+        public static IConfiguration InitConfiguration()
+        {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("securitySettings.json")
+                .Build();
+            return config;
+        }
+
+        public SecurityExtensionsTests()
+        {
+            this.Configuration = InitConfiguration();
+        }
+
+        [Fact]
+        public void Test_SecurityExtensions_Default_Pass()
+        {
+            //Arrange
+            var serviceCollection = new ServiceCollection();
+
+            serviceCollection.AddSecurity<DefaultAuthenticator>(this.Configuration);
+
+            var sp = serviceCollection.BuildServiceProvider();
+
+            //Act
+            var authService = sp.GetService<IAuthentication>();
+            var securityService = sp.GetService<ISecurityService>();
+
+            //Assert
+            Assert.True(authService != null);
+            Assert.IsType<DefaultAuthenticator>(authService);
+
+            Assert.True(securityService != null);
+            Assert.IsType<SecurityService>(securityService);
+        }
+
+        [Fact]
+        public void Test_SecurityExtensions_CustomUserModel_Pass()
+        {
+            //Arrange
+            var serviceCollection = new ServiceCollection();
+
+            serviceCollection.AddSecurity<CustomAuthenticator, UserModel>(this.Configuration);
+
+            var sp = serviceCollection.BuildServiceProvider();
+
+            //Act
+            var authService = sp.GetService<IAuthentication<UserModel>>();
+            var securityService = sp.GetService<ISecurityService<UserModel>>();
+
+            //Assert
+            Assert.True(authService != null);
+            Assert.IsType<CustomAuthenticator>(authService);
+
+            Assert.True(securityService != null);
+            Assert.IsType<SecurityService<UserModel>>(securityService);
+        }
+
+        [Fact]
+        public void Test_SecurityExtensions_AzureAD_Pass()
+        {
+            //Arrange
+            var serviceCollection = new ServiceCollection();
+
+            serviceCollection.AddAzureADSecurity(this.Configuration);
+
+            var sp = serviceCollection.BuildServiceProvider();
+
+            //Act
+            var azureADSecuritySettings = sp.GetService<AzureADSecuritySettings>();
+            var authAzure = sp.GetService<IAuthentication<AzureADAuthModel, AzureADResponseModel>>();
+
+            //Assert            
+            Assert.True(azureADSecuritySettings != null);
+            Assert.IsType<AzureADSecuritySettings>(azureADSecuritySettings);
+
+            var securitySettings = new SecuritySettings();
+            this.Configuration.Bind("SecuritySettings", securitySettings);
+
+            Assert.True(azureADSecuritySettings.AADInstance == securitySettings.AzureADSecuritySettings.AADInstance);
+            Assert.True(azureADSecuritySettings.Tenant == securitySettings.AzureADSecuritySettings.Tenant);
+            Assert.True(azureADSecuritySettings.ResourceId == securitySettings.AzureADSecuritySettings.ResourceId);
+            Assert.True(azureADSecuritySettings.ClientId == securitySettings.AzureADSecuritySettings.ClientId);
+            Assert.True(azureADSecuritySettings.ClientSecret == securitySettings.AzureADSecuritySettings.ClientSecret);
+            Assert.True(azureADSecuritySettings.APIKey == securitySettings.AzureADSecuritySettings.APIKey);
+
+            Assert.True(authAzure != null);
+            Assert.IsType<AzureAuthenticator>(authAzure);
+        }
+
+        [Fact]
+        public void Test_SecurityExtensions_Facebook_Pass()
+        {
+            //Arrange
+            var serviceCollection = new ServiceCollection();
+
+            serviceCollection.AddFacebookSecurity(this.Configuration);
+
+            var sp = serviceCollection.BuildServiceProvider();
+
+            //Act
+            var authFacebook = sp.GetService<IAuthentication<FacebookAuthModel>>();
+            var securityService = sp.GetService<ISecurityService<FacebookAuthModel>>();
+
+            //Assert            
+            Assert.True(authFacebook != null);
+            Assert.IsType<FacebookAuthenticator>(authFacebook);
+
+            Assert.True(securityService != null);
+            Assert.IsType<SecurityService<FacebookAuthModel>>(securityService);
+        }
+    }
+}
