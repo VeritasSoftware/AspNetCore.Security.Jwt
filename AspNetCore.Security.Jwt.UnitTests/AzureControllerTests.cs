5dcda0589efc01cc1968ce10b19fe5ee78290e90
VeritasSoftware <shantanu_negi@hotmail.com>
2018-12-01 12:49:48 +1100

    added azure controller unit tests.

--- AspNetCore.Security.Jwt.UnitTests/AzureControllerTests.cs
+++ AspNetCore.Security.Jwt.UnitTests/AzureControllerTests.cs
@@ -9,14 +9,14 @@ namespace AspNetCore.Security.Jwt.UnitTests
     public class AzureControllerTests
     {
         private Mock<AzureClient> MockAzureClient { get; set; }
-        private AzureADSecuritySettings securitySettings { get; set; }
+        private AzureADSecuritySettings SecuritySettings { get; set; }
 
-        internal Mock<AzureClient> InitMockAzureClient(AzureADSecuritySettings securitySettings)
+        internal Mock<AzureClient> InitMockAzureClient(AzureADSecuritySettings securitySettings, bool isAuthenticated = true)
         {            
             var securityClient = new Mock<AzureClient>(securitySettings);
             securityClient.Setup(x => x.PostSecurityRequest()).ReturnsAsync(() => new AzureADResponseModel
             {
-                IsAuthenticated = true,
+                IsAuthenticated = isAuthenticated,
                 AccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiSm9obiBEb2UiLCJleHAiOjE1NDM2MzAzNzcsIm5iZiI6MTU0MzYyNjA1NywiaXNzIjoieW91ciBhcHAiLCJhdWQiOiJ0aGUgY2xpZW50IG9mIHlvdXIgYXBwIn0.8dv4RankRw7n4OoTmqWMNFWnaTjXzobz_mUkfTh13zI"
             });
 
@@ -43,9 +43,9 @@ namespace AspNetCore.Security.Jwt.UnitTests
                 }
             };
 
-            this.securitySettings = securitySettings.AzureADSecuritySettings;
+            this.SecuritySettings = securitySettings.AzureADSecuritySettings;
 
-            this.MockAzureClient = this.InitMockAzureClient(this.securitySettings);
+            this.MockAzureClient = this.InitMockAzureClient(this.SecuritySettings);
         }
 
         [Fact]
@@ -57,7 +57,7 @@ namespace AspNetCore.Security.Jwt.UnitTests
                 APIKey = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
             };
 
-            AzureAuthenticator azureAuthenticator = new AzureAuthenticator(this.securitySettings, this.MockAzureClient.Object);
+            AzureAuthenticator azureAuthenticator = new AzureAuthenticator(this.SecuritySettings, this.MockAzureClient.Object);
 
             var controller = new AzureController(azureAuthenticator);
 
@@ -67,6 +67,30 @@ namespace AspNetCore.Security.Jwt.UnitTests
             //Assert
             Assert.IsType<ObjectResult>(result);
             Assert.True((result as ObjectResult).Value.ToString().IsValidJwtToken());
+        }
+
+        [Fact]
+        public async Task Test_AzureController_AzureAuth_Fail()
+        {
+            //Arrange
+
+            //Azure Client returns IsAuthenticated false
+            this.MockAzureClient = this.InitMockAzureClient(this.SecuritySettings, false);
+
+            AzureADAuthModel azureADAuthModel = new AzureADAuthModel
+            {
+                APIKey = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+            };
+
+            AzureAuthenticator azureAuthenticator = new AzureAuthenticator(this.SecuritySettings, this.MockAzureClient.Object);
+
+            var controller = new AzureController(azureAuthenticator);
+
+            //Act
+            var result = await controller.Create(azureADAuthModel);
+
+            //Assert
+            Assert.IsType<BadRequestResult>(result);
         }
 
         [Fact]
@@ -80,7 +104,7 @@ namespace AspNetCore.Security.Jwt.UnitTests
                 APIKey = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
             };
 
-            AzureAuthenticator azureAuthenticator = new AzureAuthenticator(this.securitySettings, this.MockAzureClient.Object);
+            AzureAuthenticator azureAuthenticator = new AzureAuthenticator(this.SecuritySettings, this.MockAzureClient.Object);
 
             var controller = new AzureController(azureAuthenticator);
 
